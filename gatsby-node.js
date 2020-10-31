41516e2a51b4267c8662dedb9ac01018a4c86ad6
KapilBishnoi <kapilbishnoi@gmail.com>
2020-10-31 01:39:56 -0700

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,5 @@
-const path = require('path')
-/*
-module.exports.onCreateNode = ({node, actions}) => {
-    const { createNodeField } = actions
+const path = require('path');
 
-    if (node.internal.type === 'MarkdownRemark'){
-        const slug = path.basename(node.fileAbsolutePath, '.md')
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        })
-    }
-}
-*/
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
 
