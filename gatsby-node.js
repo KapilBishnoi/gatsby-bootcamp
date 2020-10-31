47b7fd53d9c9c82f7934f7019678edd748f21d2e
KapilBishnoi <kapilbishnoi@gmail.com>
2020-10-31 01:46:09 -0700

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,18 @@
-const path = require('path');
+const path = require('path')
 
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
-
     const blogTemplate = path.resolve('./src/templates/blog.js')
-
     const res = await graphql(`
         query {
             allContentfulBlogPost {
                 edges {
                     node {
                         slug
+                    }
+                }
             }
-          }
         }
-      }
     `)
 
     res.data.allContentfulBlogPost.edges.forEach((edge) => {
