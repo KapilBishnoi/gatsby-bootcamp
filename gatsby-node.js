4d808461f7d9e9658ee6c3992cd23bd4ca7397d0
KapilBishnoi <kapilbishnoi@gmail.com>
2020-10-31 14:47:32 -0700

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,32 @@
-import { resolve } from 'path'
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+        query {
+            allContentfulBlogPost {
+                edges {
+                    node {
+                        slug
+                    }
+                }
+            }
+        }
+  `)
 
+  if (result.errors) {
+    reporter.panic("Error loading lessons", JSON.stringify(result.errors))
+  }
+
+  result.data.allContentfulBlogPost.edges.forEach(edge => {
+    actions.createPage({
+      path: `/blog/${edge.node.slug}`,
+      component: require.resolve("./src/templates/blog.js"),
+      context: {
+        slug: edge.node.slug,
+      },
+    })
+  })
+}
+
+/*
 export async function createPages({ graphql, actions }) {
     const { createPage } = actions
     const blogTemplate = resolve('./src/templates/blog.js')
@@ -25,3 +52,4 @@ export async function createPages({ graphql, actions }) {
         })
     })
 }
+*/
